b338a243998a8de579d5923fb3c5eb39c22b1103
dev-dongwon <dev.dongwonkim@gmail.com>
2019-08-13 20:59:41 +0900

    Refactor : interface 대신 class 사용
    접근제한자 적용한 클래스 프로퍼티, 컨트스턱터 생성

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  private age: number;
+  public gender: string;
+
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name : "tuna",
-  gender: "Male",
-  age: 31
+const tuna = new Human("tuna", 25, "Male");
+
+const sayHi = (person: Human): string => {
+  return `Hello, ${person.name}, you are ${person.age}, ${person.gender}`;
 }
 
-const sayHi = (person: Human) => {
-  console.log(`Hello, ${person.name}, you are ${person.age}, ${person.gender}`)
-}
-
-sayHi(person);
+sayHi(tuna);
 
 export {};

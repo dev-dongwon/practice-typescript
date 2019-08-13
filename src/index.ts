e5194cd4fe9b4f6c364393202fdf8992ea41306a
dev-dongwon <dev.dongwonkim@gmail.com>
2019-08-13 21:26:55 +0900

    Feat : blockchain 생성 hash 함수 구현
    hash 함수 구현 및 ADT 구현

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
+import * as CryptoJS from 'crypto-js';
+
 class Block {
+  public static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string,
+    ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString()
+
   public index: number;
   public hash: string;
   public previousHash: string;
@@ -11,23 +21,23 @@ class Block {
     previousHash: string,
     data: string,
     timestamp: number,
-    ) {
-    this.index = index;
-    this.hash = hash;
-    this.previousHash = previousHash;
-    this.data = data;
-    this.timestamp = timestamp;
-  }
+) {
+  this.index = index;
+  this.hash = hash;
+  this.previousHash = previousHash;
+  this.data = data;
+  this.timestamp = timestamp;
+}
 }
 
 const genesisBlock: Block = new Block(0, "asdfasdf", "", "hello", 123456);
 
 let blockchain: [Block] = [genesisBlock];
 
-/* this is error occured! */
-// blockchain.push("asdfasdf");
+const getBlockchain = (): Block[] => blockchain;
 
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 
-console.log(blockchain);
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
 
 export {};
